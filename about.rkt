0bbdfb5aa8cfdb9402aba0f8cd895170179070ae
flodobeutlin <flodobeutlin@mailbox.org>
2022-09-06 19:14:29 +0200

    fix external links

--- about.rkt
+++ about.rkt
@@ -13,15 +13,23 @@
        (abbr 'title: "Friedrich-Alexander-Universität Erlangen-Nürnberg" "FAU") ".")
     (p "My username " (em "flodobeutlin") " is a (arguably terrible) pun comprised of a portmanteau of " (em "Frodo") " and my nickname " (em "Flo") " as well as " (em "Beutlin") ", the german version of the last name Baggins in " (abbr 'title: "Lord Of The Rings" "LOTR") "." )
     (p "Next to this site I also have a "
-       (ll #:href "https://wwwcip.cs.fau.de/~oc45ujef" #:name "university website")
+       (ll #:href "https://wwwcip.cs.fau.de/~oc45ujef"
+           #:name "university website"
+           #:external #t)
        " where I host miscellaneous content related to my studies."))
    (section
     (h3 "this page")
     (p "This website is generated using a collection of racket scripts based on the great "
-       (ll #:href "https://docs.racket-lang.org/scribble" #:name (code "#lang scribble"))
+       (ll #:href "https://docs.racket-lang.org/scribble"
+           #:name (code "#lang scribble")
+           #:external #t)
        " , though I only use it as a library for generating html from s-expressions."
-       " Find the source " (ll #:href "https://github.com/flodobeutlin/blog" #:name "here") ".")
+       " Find the source " (ll #:href "https://github.com/flodobeutlin/blog"
+                               #:name "here"
+                               #:external #t) ".")
     (p "The color scheme(s) are shamelessly ripped of emacs's "
-       (ll #:href "https://github.com/protesilaos/modus-themes" #:name (code "modus-themes")) "."))))
+       (ll #:href "https://github.com/protesilaos/modus-themes"
+           #:name (code "modus-themes")
+           #:external #t) "."))))
 
 (output (site content))
